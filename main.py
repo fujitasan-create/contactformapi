f46b1686d215fd120c08bd26ab749f008ac845c3
fujitasan-create <blackokayu@yahoo.co.jp>
2025-06-07 09:57:52 +0900

    セキュリティ

--- main.py
+++ main.py
@@ -1,9 +1,32 @@
-from fastapi import FastAPI, Depends
+from fastapi import FastAPI, Depends, HTTPException, status
+from fastapi.security import HTTPBasic,HTTPBasicCredentials
 from sqlalchemy.orm import Session
 from pydantic import BaseModel
-
 from database import SessionLocal,engine
 import models
+import secrets
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+BASIC_AUTH_USERNAME = os.getenv("BASIC_AUTH_USERNAME")
+BASIC_AUTH_PASSWORD = os.getenv("BASIC_AUTH_PASSWORD")
+
+security = HTTPBasic()
+
+def get_current_username(credentials: HTTPBasicCredentials = Depends(security)):
+
+    is_correct_username = secrets.compare_digest(credentials.username, BASIC_AUTH_USERNAME)
+    is_correct_password = secrets.compare_digest(credentials.password, BASIC_AUTH_PASSWORD)
+
+    if not (is_correct_username and is_correct_password):
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="認証に失敗しました",
+            headers={"WWW-Authenticate": "Basic"},
+        )
+    return credentials.username
 
 models.Base.metadata.create_all(bind=engine)
 app= FastAPI()
@@ -30,6 +53,9 @@ def create_contact(contact: ContactCreate, db: Session = Depends(get_db)):
     return {"message":"お問い合わせを受け付けました"}
 
 @app.get("/contacts")
-def read_contacts(db: Session = Depends(get_db)):
+def read_contacts(
+    db: Session = Depends(get_db),
+    username: str = Depends(get_current_username)  
+):
     contacts = db.query(models.Contact).all()
     return contacts
