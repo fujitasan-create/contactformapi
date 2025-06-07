95b2b0e884fd63da0b5f206aeceb9a2f7e7b1b2f
fujitasan-create <blackokayu@yahoo.co.jp>
2025-06-07 15:40:27 +0900

    origin追加

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI, Depends, HTTPException, status
 from fastapi.security import HTTPBasic,HTTPBasicCredentials
+from fastapi.middleware.cors import CORSMiddleware
 from sqlalchemy.orm import Session
 from pydantic import BaseModel
 from database import SessionLocal,engine
@@ -30,6 +31,19 @@ def get_current_username(credentials: HTTPBasicCredentials = Depends(security)):
 
 models.Base.metadata.create_all(bind=engine)
 app= FastAPI()
+
+origins = [
+    "https://fujitasan-create.github.io",  
+    "http://localhost:3000"                
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 def get_db():
     db= SessionLocal()
