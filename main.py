01d6738583c51179d7a36f910b7b4f58d2fbcb20
fujitasan-create <blackokayu@yahoo.co.jp>
2025-06-07 16:30:13 +0900

    修正

--- main.py
+++ main.py
@@ -34,12 +34,12 @@ app= FastAPI()
 
 origins = [
     "https://fujitasan-create.github.io",  
-    "http://localhost:3000"                
+    "http://localhost:3000",                
 ]
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins,
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
